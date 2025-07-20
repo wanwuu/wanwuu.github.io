58faa8f775ea65902739a04cc0c3e6d089146a04
91nantong <91nantong@gmail.com>
2025-07-20 14:48:53 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'owutejg.com',
+	'qmylctfe.club',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
