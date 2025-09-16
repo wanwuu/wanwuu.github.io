ac4aeae51d5cf5300d71d7d495416bfd65aa8d66
91nantong <91nantong@gmail.com>
2025-09-16 17:03:38 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'pokhauev.xyz',
+	'vgrlialp.cc',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
