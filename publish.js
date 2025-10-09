9acf8170c8cbe0e997f4c0600f2bfc7d99d2e2c1
91nantong <91nantong@gmail.com>
2025-10-09 17:18:34 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'fkbqbfbd.cc',
+	'ihpsaens.com',
 ];
                                                                                                                   
 var JumpPage="https://91nt.com";
