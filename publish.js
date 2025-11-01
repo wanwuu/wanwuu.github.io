b1645905bf082b7d0d99309a4819ac8a0e70f18b
wanwuu <c28388232@gmail.com>
2025-11-01 17:15:08 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'dyxemlv.cc',
+	'dyxobic.cc',
 ];
                                                                                                                   
 var JumpPage="https://wanwuu.com";
